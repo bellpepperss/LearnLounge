6f40fd01439db6b26c0d9ca049ad6153d5a6ea0c
belbel <you@example.comanabelvaleria16@gmail.com>
2024-05-14 21:47:36 +0700

    Added animation to sidenav

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("sidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("sidenav").style.width = "100px";
+    document.getElementById("main").style.marginLeft = "100px";
+  }
