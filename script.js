2fb7833a2636b513d612fe7755dd328ee28f8237
belbel <you@example.comanabelvaleria16@gmail.com>
2024-05-17 21:09:23 +0700

    Fix hover animation

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 function openNav() {
     document.getElementById("sidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("topnav").style.marginLeft = "250px";
   }
   
   /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
   function closeNav() {
     document.getElementById("sidenav").style.width = "100px";
     document.getElementById("main").style.marginLeft = "100px";
+    document.getElementById("topnav").style.marginLeft = "100px";
   }
